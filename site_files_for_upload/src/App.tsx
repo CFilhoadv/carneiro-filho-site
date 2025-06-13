c73b93ce86717e0b095b18163cff52116bed1740
CFilhoadv <fcfcfilho@gmail.com>
2025-06-12 23:18:11 -0300

    Add files via upload
    Versão inicial do site

---
+++ site_files_for_upload/src/App.tsx
@@ -0,0 +1,26 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import QuemSomosPage from './pages/QuemSomosPage';
+import PlanejamentoSucessorioPage from './pages/PlanejamentoSucessorioPage';
+import ServicosPage from './pages/ServicosPage';
+import CursosPage from './pages/CursosPage';
+import ContatoPage from './pages/ContatoPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quem-somos" element={<QuemSomosPage />} />
+        <Route path="/planejamento-sucessorio" element={<PlanejamentoSucessorioPage />} />
+        <Route path="/servicos" element={<ServicosPage />} />
+        <Route path="/cursos" element={<CursosPage />} />
+        <Route path="/contato" element={<ContatoPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
