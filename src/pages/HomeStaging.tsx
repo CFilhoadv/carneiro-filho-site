6774237de7400683f4d674ff49d6a1c01caab942
CFilho Advocacia <fcfcfilho@gmail.com>
2025-12-22 14:34:24 -0300

    fix: correção do path de importação do banner na Home

--- src/pages/HomeStaging.tsx
+++ src/pages/HomeStaging.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import MainLayout from '../components/layout/MainLayout';
-import bannerImg from '../../assets/images/banner_familia.webp';
+import bannerImg from '../assets/images/banner_familia.webp';
 
 const HomeStaging: React.FC = () => {
   return (
